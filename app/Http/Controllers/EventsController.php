71a6d4fbc8827203dc99ff07c6058515fced39df
Christian <chrisdaq@gmail.com>
2024-03-15 18:14:16 -0400

    create a new request object, use it on controller, the validate method is called under the hood

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Http\Requests\StoreEventRequest;
+use Illuminate\Http\RedirectResponse;
 
 class EventsController extends Controller
 {
-    public function store(Request $request)
+    public function store(StoreEventRequest $request): RedirectResponse
     {
         $event = $request->all();
         Event::create($event);
